7b94e0e6b2d667eab19fb925e21813b6a481e03c
Mingyeol Kim <neomingyeol@gmail.com>
2025-06-16 17:27:45 +0900

    Init

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T> where T : MonoBehaviour
+{
+    private Queue<T> pool = new Queue<T>();
+    private T prefab;
+    private Transform parent;
+
+    public ObjectPool(T prefab, int initialSize, Transform parent = null)
+    {
+        this.prefab = prefab;
+        this.parent = parent;
+
+        for (int i = 0; i < initialSize; i++)
+        {
+            T obj = GameObject.Instantiate(prefab, parent);
+            obj.gameObject.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    public T Get()
+    {
+        T obj;
+
+        if (pool.Count > 0)
+        {
+            obj = pool.Dequeue();
+        }
+        else
+        {
+            obj = GameObject.Instantiate(prefab, parent);
+        }
+
+        obj.gameObject.SetActive(true);
+        return obj;
+    }
+
+    public void Return(T obj)
+    {
+        obj.gameObject.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
